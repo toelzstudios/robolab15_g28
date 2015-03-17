0870a4d1dfd0675f417843c9d311ae3b79efb627
thomhens <thomhens@gmx.de>
2015-03-17 17:47:07 +0100

    added robot
    Robotersteuerung

---
+++ Roboterabteil/workspace_robot/example/kernel_id.h
@@ -0,0 +1,32 @@
+/*
+ *  kernel_id.h
+ *  Tue Mar 17 14:15:49 2015
+ *  SG Version 2.10
+ *  sg.exe ./main.oil -os=ECC2 -IC:/cygwin/nxtOSEK/toppers_osek/sg/impl_oil -template=C:/cygwin/nxtOSEK/toppers_osek/sg/lego_nxt.sgt
+ */
+#ifndef KERNEL_ID_H
+#define KERNEL_ID_H
+
+ /****** Object OS ******/
+
+#define USE_STARTUPHOOK
+#define USE_SHUTDOWNHOOK
+
+ /****** Object TASK ******/
+
+ /****** Object COUNTER ******/
+
+ /****** Object ALARM ******/
+
+ /****** Object RESOURCE ******/
+
+ /****** Object EVENT ******/
+
+ /****** Object ISR ******/
+
+ /****** Object APPMODE ******/
+
+#define appmode1	(AppModeType)( 1U << 0 )
+
+#endif	/* ! KERNEL_ID_H */
+
