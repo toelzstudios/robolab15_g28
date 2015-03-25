efea61731371bca782f925962422a01d04392c52
henning <h_i_@online.de>
2015-03-25 10:44:03 +0100

    alte Ordner entfernt

--- Roboterabteil/workspace_robot/example/rxeflash.sh
+++
@@ -1,4 +0,0 @@
-echo Executing NeXTTool to upload main.rxe...
- /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -download=main.rxe
- /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -listfiles=main.rxe
-echo NeXTTool is terminated.
